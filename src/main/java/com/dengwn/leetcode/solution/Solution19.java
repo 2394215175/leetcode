afc169b4fdb2aa07dbe7f14a7aad3b5f52d6f77e
dengwn <2394215175@qq.com>
2022-12-27 17:27:20 +0800

    2022-12-27日提交2

---
+++ src/main/java/com/dengwn/leetcode/solution/Solution19.java
@@ -0,0 +1,35 @@
+package com.dengwn.leetcode.solution;
+
+/**
+ * @author: dengwn
+ * @date: 2022-12-27
+ **/
+public class Solution19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode virtual = new ListNode(0, head);
+        ListNode slow = virtual;
+        ListNode fast = head;
+        while (n > 1 && fast != null) {
+            fast = fast.next;
+            n--;
+        }
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return virtual.next;
+    }
+
+    public static void main(String[] args) {
+//        ListNode node5 = new ListNode(5);
+//        ListNode node4 = new ListNode(4, node5);
+//        ListNode node3 = new ListNode(3, node4);
+//        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1);
+        Solution19 solution19 = new Solution19();
+        System.out.println(solution19.removeNthFromEnd(node1, 1).val);
+    }
+}
+
