ddea5bf7693851bda8eafe17edd3351a7d226602
dengwn <2394215175@qq.com>
2022-12-27 14:07:49 +0800

    2022-12-27日提交1

---
+++ src/main/java/com/dengwn/leetcode/solution/Solution189.java
@@ -0,0 +1,17 @@
+package com.dengwn.leetcode.solution;
+
+import java.util.Arrays;
+
+/**
+ * @author: dengwn
+ * @date: 2022-12-26
+ **/
+public class Solution189 {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] copy = Arrays.copyOf(nums, n);
+        for (int i = 0; i < n; i++) {
+            nums[i] = copy[(10000 * n - k + i) % n];
+        }
+    }
+}
---
+++ src/main/java/com/dengwn/leetcode/solution/Solution70.java
@@ -0,0 +1,20 @@
+package com.dengwn.leetcode.solution;
+
+/**
+ * @author: dengwn
+ * @date: 2022-12-23
+ **/
+public class Solution70 {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        if (n <= 2) {
+            return n;
+        }
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
